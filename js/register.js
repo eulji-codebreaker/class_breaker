170b42cb5b87b38554479b81050a096990e834f3
jia40 <lsc7439@g.eulji.ac.kr>
2026-03-24 17:04:37 +0900

    project

--- js/register.js
+++ js/register.js
@@ -3,9 +3,11 @@ fetch("../layout/header.html") //웹문서 요청
     .then((responseText) => {
         // console.log(responseText);
         document.querySelector('body').insertAdjacentHTML("afterbegin", responseText);
+        document.querySelector('#headerLogo')
+            .onclick = () => location.href = '/mainPage/index.html';
         const day = document.querySelector('#dday');
 
-            function diffDay() {
+        function diffDay() {
             const getDay = new Date(2026, 3, 21); //설정하고자 하는 달에서 -1로 설정해야 함
             const today = new Date();
             const diff = getDay - today;
@@ -14,10 +16,9 @@ fetch("../layout/header.html") //웹문서 요청
             day.innerText = `중간고사 D-${days}`;
         }
 
-            diffDay();
-            setInterval(diffDay, 1000 * 60 * 60);
-    })
-    
+        diffDay();
+        setInterval(diffDay, 1000 * 60 * 60);
+    });
 fetch("../layout/footer.html") //웹문서 요청
     .then((response) => response.text()) //응답을 받고 text로 변환
     .then((responseText) => {
